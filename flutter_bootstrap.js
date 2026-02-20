18d68fb30e2e13ada8dd9133717db6d7ec46d536
jedrek7575 <jedrek7575@mail.com>
2026-02-20 19:30:36 +0000

    Update from https://github.com/jedrek7575/MLogistyk/commit/d17b2963b25ec13e7370b4bdf2196677e705ff7b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"6c0baaebf70e0148f485f27d5616b3d3382da7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1970835570" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3825138934" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
